6905a28c4d2e802812ef10b1e462f25b252a3dd0
Matheesha Weerawansha <matheeshaweerawansha32@gmail.com>
2026-03-08 14:51:44 +0530

    Refactor database connection logic and improve logging in db.js; initialize connection in server.js

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log("✅ MongoDB connected successfully");
   } catch (error) {
-    console.error('Database connection failed:', error.message);
+    console.error("❌ Database connection failed:", error);
     process.exit(1);
   }
 };
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("ORIONX API is officially running!");
